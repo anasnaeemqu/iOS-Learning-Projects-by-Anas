6bd7f99c9732ab63c4be9bfc107cf33d6de7395c
anasnaeemqu <136015453+anasnaeemqu@users.noreply.github.com>
2026-01-06 20:46:44 +0500

    DiceRollerFile

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Meezotech Mac on 01/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -0,0 +1,34 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Meezotech Mac on 01/01/2026.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    
+   @State private var numberOfPips: Int = 2
+    
+    var body: some View {
+        VStack{
+            Image(systemName: "die.face.\(numberOfPips).fill")
+                .resizable()
+                .frame(maxWidth: 100, maxHeight: 100)
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundStyle(.black, .white)
+            
+            Button("Roll") {
+                withAnimation{
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+}
+
+#Preview {
+    DiceView()
+}
